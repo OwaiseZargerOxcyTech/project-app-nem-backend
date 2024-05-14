ea7e06151f2757496d76fe9cde80b60b293c8652
ZahidKhanOxytech <zahid.khan@oxytech.com>
2024-05-14 13:52:23 +0530

    Added fronend vercel url in cors

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://project-app-nem-frontend.vercel.app",
+    allowedHeaders: ["Accept", "Content-Type", "Authorization"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(bodyParser.json());
